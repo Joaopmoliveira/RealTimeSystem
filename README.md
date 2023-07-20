2b748a95d4841fc17b8390bc6970f10242dd85a1
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2023-07-20 11:05:01 +0100

    Update readme and sensors code

--- README.md
+++ README.md
@@ -20,22 +20,149 @@ file describing the message which must be shared accross processes.
 
 ```json
 {
-    "shared_memory_name" : "MyCustomName",
+    "shared_memory_name" : "MEMORY_WATCHDOG",
     "messages" : [
         {
         "message" : "gps_reading",
         "fields" : [
-            {"name" : "position", "type" : "double", "array" : 3},
-            {"name" : "error_state", "type" : "size_t" , "array" : 1},
-            {"name" : "image", "type" : "bytes" , "array" : 1000},
+            {"name" : "counter", "type" : "int", "array" : 1},
+            {"name" : "latitude", "type" : "double" , "array" : 1},
+            {"name" : "longitude", "type" : "double" , "array" : 1},
+            {"name" : "height", "type" : "double" , "array" : 1},
+            {"name" : "velocity", "type" : "double" , "array" : 3},
+            {"name" : "acceleration", "type" : "double" , "array" : 3},
+            {"name" : "gforce", "type" : "double" , "array" : 1},
+            {"name" : "orientation", "type" : "double" , "array" : 3},
+            {"name" : "angular_velocity", "type" : "double" , "array" : 3},
+            {"name" : "standard_deviation", "type" : "double" , "array" : 3}
         ]
+        },
+        {
+        "message" : "grayscale_image_1",
+        "fields" : [
+            {"name" : "counter", "type" : "int", "array" : 1},
+            {"name" : "data", "type" : "bytes", "array" : 5880000 }
+        ]  
+        },
+        {
+        "message" : "rgb_image_1",
+        "fields" : [
+            {"name" : "counter", "type" : "int", "array" : 1},
+            {"name" : "data", "type" : "bytes", "array" : 5880000 }
+        ]  
         }
-    ]
+        ]
 }
 ```
 
+The compiler takes the name of the shared memory, and name of the messages and automatically computes the necessary size for the shared memory, the adresses on this shared memory and methods to read and write our messages unto the shared memory.
+
 ## WatchDog
+
+The watchdog is the process which guaraantees that the sample time is respected irregardless of the workload. To do this we use assyncronous calls as much as possible and set a timer to expire at a latter point in time. If the control loop
+1. Command Control Law
+2. Request readings to shared memory
+3. Read readings into shared memory
+4. Warn client code of available readings
+5. Receive control actions 
+
+does not finish before the timer expires the watchdog executes a safety stop guarantying that the sample time is always respected.
+
 
 ## Client
 
+The client code is oblivious to the timing requirements. It works on a best effort basis, trying to execute as fast as possible the readings from the sensors, the necessary post-processing and the control law. If the sample time is not respected the application will have its sockets throwing an exception warning that the communication with the watchdog has failed.
+
 ## Sensors
+
+The sensors code is quite involded, but once you understand the structure, it will become easier to write clean and safe code which always works. Lets go into what are the requirements of our application and how we can achieve these requirements
+
+1. The sensor code must receive a request from the watchdog. Once this request is received it must request readings from the peripherals, i.e. cameras, gps, imus, etcs..
+
+2. Once all the peripherals have given us their reading we must warn the watchdog that we have finished all our requirements
+
+3. The minimum possible delay should exist between executing readings between the peripherals and writing this information to the shared memory
+
+To achieve these requirements we propose an architecture where each peripheral interacts with a dedicated thread. This thread should execute in a loop the readings in a best effort approach. To do this we use a Sincronizer object, defined according to 
+
+```cpp
+enum class Peripheral{
+    GPS_READING = 0,
+    CAMERA = 1,
+    COUNT = 2
+};
+
+struct Sincronizer{
+    std::atomic<bool> valid = false;
+    std::array<std::atomic<bool>,static_cast<int>(Peripheral::COUNT)> flags;
+    std::mutex mut;
+    std::condition_variable cv;
+    size_t written = 0;
+
+    template<Peripheral index>
+    inline bool should_write(){
+        static_assert(static_cast<int>(index)<static_cast<int>(Peripheral::COUNT),"the maximum index to read must be smaller or equal than the number of peripherals");
+        if(flags[static_cast<int>(index)]){
+            flags[static_cast<int>(index)] = false;
+            return true;
+        }
+        return false;
+    };
+
+    template<Peripheral... args>
+    void write(){
+        constexpr size_t number_of_args = sizeof...(args);
+        internal_write<args...>();
+        wait<number_of_args>();
+    }
+
+    inline void wrote(){
+        std::lock_guard<std::mutex> g{mut};
+        ++written;
+        cv.notify_one();
+    };
+
+    inline void stop(){
+        valid.store(true,std::memory_order_relaxed);
+    };
+
+    inline bool is_stoped(){
+        return valid.load(std::memory_order_relaxed);
+    };
+
+private:
+    template<Peripheral index,Peripheral... args>
+    void internal_write(){
+        static_assert(index!=Peripheral::COUNT,"COUNT is not a valid peripheral, it is used for internal purpouses");
+        flags[static_cast<int>(index)] = true;
+        if constexpr (sizeof...(args)>0)
+            internal_write<args...>();
+    };
+
+    template<size_t number_of_args>
+    inline void wait(){
+        std::unique_lock<std::mutex> lk{mut};
+        cv.wait(lk,[this](){return written == number_of_args;});
+        written = 0;
+    };
+
+};
+
+```
+
+Although this class looks and feels convoluted, it is actually simple to use, with strong guarantees about safety. Here is a simple example showcasing how this class can be used. 
+
+```cpp
+int gps_reader(){
+
+};
+
+
+int camera_reader(){
+
+};
+
+int main(){
+    return 0;
+};
+```
--- sensors.cpp
+++ sensors.cpp
@@ -1,68 +1,151 @@
-#include <asio.hpp>
-#include <iostream>
-#include <iomanip>
-#include <stdexcept>
-#include <string>
-#include <utility>
-#include "message_sizes.h"
 #include <array>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+#include <iostream>
+#include <csignal>
+#include <thread>
+#include <sstream>
+#include <asio.hpp>
+#include <cmath>
+#include <type_traits>
 
-int main(int argc, char* argv[]){
-if(argc!=4){
-    std::cout << "To call this executable provide 3 arguments \n- ip , e.g. \"localhost\" \n- port , e.g. 30000\n- server of watchdog , e.g. 15000" << std::endl;
-    return 1;
-  }
+struct printer{
+    std::mutex mut;
+    template<typename T>
+    printer& operator << (T obj){
+        std::lock_guard<std::mutex> g{mut};
+        std::cout << obj;
+        return *(this);
+    }
+};
 
-    asio::io_context io_context;
-    // once the connection has been established we create a server that the client can connect himself to us. 
-    asio::error_code ec;
-    std::string string_port{argv[1]};
+printer _cout;
 
-    std::size_t pos = 0;
-    try{
-        std::cout << "std::stoi('" << string_port << "'): ";
-        const int i{std::stoi(string_port, &pos)};
-        std::cout << i << "; pos: " << pos << '\n';
-    } catch (std::invalid_argument const& ex){
-        std::cout << "std::invalid_argument::what(): " << ex.what() << '\n';
-        return 1;
-    } catch (std::out_of_range const& ex){
-        std::cout << "std::out_of_range::what(): " << ex.what() << '\n';
-        const long long ll{std::stoll(string_port, &pos)};
-        std::cout << "std::stoll('" << string_port << "'): " << ll << "; pos: " << pos << '\n';
-        return 1;
+enum class Peripheral{
+    GPS_READING = 0,
+    CAMERA = 1,
+    COUNT = 2
+};
+
+struct Sincronizer{
+    std::atomic<bool> valid = false;
+    std::array<std::atomic<bool>,static_cast<int>(Peripheral::COUNT)> flags;
+    std::mutex mut;
+    std::condition_variable cv;
+    size_t written = 0;
+
+    template<Peripheral index>
+    inline bool should_write(){
+        static_assert(static_cast<int>(index)<static_cast<int>(Peripheral::COUNT),"the maximum index to read must be smaller or equal than the number of peripherals");
+        if(flags[static_cast<int>(index)]){
+            flags[static_cast<int>(index)] = false;
+            return true;
+        }
+        return false;
+    };
+
+    template<Peripheral... args>
+    void write(){
+        constexpr size_t number_of_args = sizeof...(args);
+        internal_write<args...>();
+        wait<number_of_args>();
     }
 
-    short port = pos;
-    asio::ip::tcp::endpoint endpoit(asio::ip::tcp::v4(), port);
-    asio::ip::tcp::acceptor acceptor(io_context);
-    asio::ip::tcp::socket client_socket(io_context);
-    acceptor.accept(client_socket,endpoit,ec);
-    if (ec){
-        return 1;
+    inline void wrote(){
+        std::lock_guard<std::mutex> g{mut};
+        ++written;
+        cv.notify_one();
     };
 
-    ClientObservationsMessage observations;
-    ClientControlLawMessageHeader header;
-    ClientControlLawMessage body;
-    std::array<unsigned char,buffer_size> mega_buffer;
-
-    while(true){
-        size_t message_size = 0;
-        pack_observation_message(observations,mega_buffer.data(),message_size);
-        asio::write(client_socket,asio::buffer(mega_buffer),asio::transfer_exactly(message_size),ec);
-
-        //we want to go as fast as possible, no sleeping in here
-        asio::read(client_socket,asio::buffer(mega_buffer),asio::transfer_exactly(ClientControlLawMessageHeader::client_control_law_header_size),ec);
-        if(ec && !unpack_control_law_header(mega_buffer.data(),ClientControlLawMessageHeader::client_control_law_header_size,header)){
-            std::cout << "watchdog stopped the connection\n";
-            return 1;
-        }
-        asio::read(client_socket,asio::buffer(mega_buffer),asio::transfer_exactly(header.size_of_control_law),ec);
-        if(ec || !unpack_control_law_message(mega_buffer.data(),header.size_of_control_law,body)){
-            std::cout << "watchdog stopped the connection\n";
-            return 1;
-        }
+    inline void stop(){
+        valid.store(true,std::memory_order_relaxed);
     };
-    return 0;
+
+    inline bool is_stoped(){
+        return valid.load(std::memory_order_relaxed);
+    };
+
+private:
+    template<Peripheral index,Peripheral... args>
+    void internal_write(){
+        static_assert(index!=Peripheral::COUNT,"COUNT is not a valid peripheral, it is used for internal purpouses");
+        flags[static_cast<int>(index)] = true;
+        if constexpr (sizeof...(args)>0)
+            internal_write<args...>();
+    };
+
+    template<size_t number_of_args>
+    inline void wait(){
+        std::unique_lock<std::mutex> lk{mut};
+        cv.wait(lk,[this](){return written == number_of_args;});
+        written = 0;
+    };
+
+};
+
+void camera_reader(Sincronizer& sincronizer,std::chrono::steady_clock::time_point begin){
+    while(!sincronizer.is_stoped()){
+        if(sincronizer.should_write<Peripheral::CAMERA>()){
+            sincronizer.wrote();
+            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+            std::stringstream ss;
+            ss << "Camera = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+            _cout << ss.str();
+        }
+    }
+}
+
+void gps_reader(Sincronizer& sincronizer,std::chrono::steady_clock::time_point begin){
+    while(!sincronizer.is_stoped()){
+        // always polling for new data
+        if(sincronizer.should_write<Peripheral::GPS_READING>()){
+            sincronizer.wrote();
+            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+            std::stringstream ss;
+            ss << "GPS = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+            _cout << ss.str();
+        }
+    }
+}
+
+Sincronizer sincronizer;
+
+void signal_handler(int val)
+{
+    sincronizer.stop();
+}
+
+int main(){
+    std::signal(SIGINT,signal_handler);
+
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+    std::thread camera_thread{[&](){camera_reader(sincronizer, begin);}};
+    std::thread gps_thread{[&](){gps_reader(sincronizer, begin);}};
+    try{
+   for(size_t counter = 0;!sincronizer.is_stoped(); ++counter){
+        if(counter % 5 == 0){
+            sincronizer.write<Peripheral::CAMERA,Peripheral::GPS_READING>();
+            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+            std::stringstream ss;
+            ss << "main = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+            _cout << ss.str();
+        } else {
+            sincronizer.write<Peripheral::GPS_READING>();
+            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+            std::stringstream ss;
+            ss << "main = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+            _cout << ss.str();
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        _cout << "=============================================\n";
+    
+    }
+    }catch(...){
+        std::cout << "failure was detected in either communication or shared memory operation\n";
+    }
+ 
+    camera_thread.join();
+    gps_thread.join();
 }
